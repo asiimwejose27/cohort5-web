798ff3bfd627ff5fa53f672d791c48d0da320820
asiimwejose <asiimwejose27@gmail.com>
2021-10-11 15:08:08 +0300

    js

---
+++ js/intro.js
@@ -0,0 +1,19 @@
+console.log("Hello there, welcome to javascript")
+
+
+//variables
+var myName; 
+myName = "Josephine";
+age = 18
+console.log(myName, age)
+
+let country;
+country = "Uganda"
+console.log(country)
+
+country = "Kenya"
+console.log(country)
+
+let temperature = 28
+console.log(temperature)
+
